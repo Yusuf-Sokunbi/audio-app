ece0a45a513e6e9099bca6e4b1d4e8bf29259f0b
Yusuf <adeolaoo87@gmail.com>
2025-10-25 14:07:53 +0100

    Edit playlist

--- src/album.js
+++ src/album.js
@@ -1,6 +1,147 @@
 
 export const albums = [
+    {   id: 1,
+        title: "IOGBC",
+        artist: "dr isiaq abolore",
+        year: 2021,
+        audio: require("../src/assets/Audio/IOGBC14.mp3"),
+        cover: require("../src/assets/Image/cover1.jpg")
+    },
     {
+        id: 2,
+        title: "Tafsir Day11",
+        artist:"dr isiaq abolore",
+        year: 2020,
+        audio: require("../src/assets/Audio/Tafsir10.mp3"),
+        cover: require("../src/assets/Image/cover2.jpg")
+    },
+    {
+        id: 3,
+        title: "Tafsir Day12",
+        artist:"dr isiaq abolore",
+        year: 2019,
+        audio: require("../src/assets/Audio/Tafsir12.mp3"),
+        cover: require("../src/assets/Image/cover3.webp")
+    },
+      {
+        id: 4,
+        title: "Tafsir Day13",
+        artist:"dr isiaq abolore",
+        year: 2019,
+        audio: require("../src/assets/Audio/Tafsir13.mp3"),
+        cover: require("../src/assets/Image/cover4.webp")
+    },
+      {
+        id: 5,
+        title: "Tafsir Day14",
+        artist:"dr isiaq abolore",
+        year: 2019,
+        audio: require("../src/assets/Audio/Tafsir14.mp3"),
+        cover: require("../src/assets/Image/cover5.webp")
+    },
+    {
+        id: 6,
+        title: "Tafsir Day15",
+        artist:"dr isiaq abolore",
+        year: 2019,
+        audio: require("../src/assets/Audio/Tafsir15.mp3"),
+        cover: require("../src/assets/Image/cover6.jpg")
+    },
+    {   id: 7,
+        title: "Tafsir Day16",
+        artist: "dr isiaq abolore",
+        year: 2021,
+        audio: require("../src/assets/Audio/Tafsir16.mp3"),
+        cover: require("../src/assets/Image/cover1.jpg")
+    },
+    {
+        id: 8,
+        title: "Tafsir Day19",
+        artist:"dr isiaq abolore",
+        year: 2020,
+        audio: require("../src/assets/Audio/Tafsir19.mp3"),
+        cover: require("../src/assets/Image/cover2.jpg")
+    },
+    {
+        id: 9,
+        title: "ipe ododo day1",
+        artist:"dr isiaq abolore",
+        year: 2019,
+        audio: require("../src/assets/Audio/IPEODODO.mp3"),
+        cover: require("../src/assets/Image/cover3.webp")
+    },
+      {
+        id: 10,
+        title: "ipe ododo day11",
+        artist:"dr isiaq abolore",
+        year: 2019,
+        audio: require("../src/assets/Audio/IPEODODO11.mp3"),
+        cover: require("../src/assets/Image/cover4.webp")
+    },
+      {
+        id: 11,
+        title: "ipe ododo day12",
+        artist:"dr isiaq abolore",
+        year: 2019,
+        audio: require("../src/assets/Audio/IPEODODO12.mp3"),
+        cover: require("../src/assets/Image/cover5.webp")
+    },
+    {
+        id: 12,
+        title: "ipe ododo day14",
+        artist:"dr isiaq abolore",
+        year: 2019,
+        audio: require("../src/assets/Audio/IPEODODO14.mp3"),
+        cover: require("../src/assets/Image/cover6.jpg")
+    },
+    {   id: 13,
+        title: "ipe ododo day16",
+        artist: "dr isiaq abolore",
+        year: 2021,
+        audio: require("../src/assets/Audio/IPEODODO16.mp3"),
+        cover: require("../src/assets/Image/cover1.jpg")
+    },
+    {
+        id: 14,
+        title: "ipe ododo day17",
+        artist:"dr isiaq abolore",
+        year: 2020,
+        audio: require("../src/assets/Audio/IPEODODO17.mp3"),
+        cover: require("../src/assets/Image/cover2.jpg")
+    },
+    {
+        id: 15,
+        title: "ipe ododo day18",
+        artist:"dr isiaq abolore",
+        year: 2019,
+        audio: require("../src/assets/Audio/IPEODODO18.mp3"),
+        cover: require("../src/assets/Image/cover3.webp")
+    },
+      {
+        id: 16,
+        title: "ipe ododo day19",
+        artist:"dr isiaq abolore",
+        year: 2019,
+        audio: require("../src/assets/Audio/IPEODODO19.mp3"),
+        cover: require("../src/assets/Image/cover4.webp")
+    },
+      {
+        id: 17,
+        title: "ipe ododo day20",
+        artist:"dr isiaq abolore",
+        year: 2019,
+        audio: require("../src/assets/Audio/IPEODODO20.mp3"),
+        cover: require("../src/assets/Image/cover5.webp")
+    },
+    {
+        id: 18,
+        title: "ipe ododo day17",
+        artist:"dr isiaq abolore",
+        year: 2019,
+        audio: require("../src/assets/Audio/IPEODODO17.mp3"),
+        cover: require("../src/assets/Image/cover6.jpg")
+    }, 
+    {   id: 19,
         title: "ipe ododo day1",
         artist: "dr isiaq abolore",
         year: 2021,
@@ -8,38 +149,43 @@ export const albums = [
         cover: require("../src/assets/Image/cover1.jpg")
     },
     {
-        title: "ipe ododo day2",
+        id: 20,
+        title: "ipe ododo day11",
         artist:"dr isiaq abolore",
         year: 2020,
-        audio: require("../src/assets/Audio/IPEODODO.mp3"),
+        audio: require("../src/assets/Audio/IPEODODO11.mp3"),
         cover: require("../src/assets/Image/cover2.jpg")
     },
     {
-        title: "ipe ododo day3",
+        id: 21,
+        title: "ipe ododo day12",
         artist:"dr isiaq abolore",
         year: 2019,
-        audio: require("../src/assets/Audio/IPEODODO.mp3"),
+        audio: require("../src/assets/Audio/IPEODODO12.mp3"),
         cover: require("../src/assets/Image/cover3.webp")
     },
       {
-        title: "ipe ododo day4",
+        id: 22,
+        title: "ipe ododo day14",
         artist:"dr isiaq abolore",
         year: 2019,
-        audio: require("../src/assets/Audio/IPEODODO.mp3"),
+        audio: require("../src/assets/Audio/IPEODODO14.mp3"),
         cover: require("../src/assets/Image/cover4.webp")
     },
       {
-        title: "ipe ododo day5",
+        id: 23,
+        title: "ipe ododo day16",
         artist:"dr isiaq abolore",
         year: 2019,
-        audio: require("../src/assets/Audio/IPEODODO.mp3"),
+        audio: require("../src/assets/Audio/IPEODODO16.mp3"),
         cover: require("../src/assets/Image/cover5.webp")
     },
     {
-        title: "ipe ododo day6",
+        id: 24,
+        title: "ipe ododo day17",
         artist:"dr isiaq abolore",
         year: 2019,
-        audio: require("../src/assets/Audio/IPEODODO.mp3"),
+        audio: require("../src/assets/Audio/IPEODODO17.mp3"),
         cover: require("../src/assets/Image/cover6.jpg")
     },
 ]
--- src/components/PlayList.vue
+++ src/components/PlayList.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="p-3 grid grid-cols-2 gap-6 text-white ">
+    <div class="p-2 grid grid-cols-2 md:grid-cols-4 gap-2 text-white ">
        <div v-for="playlist in playlists" :key="playlist.id" class="bg-blue-500 my-6  p-4 grid place-items-center" >
         <img :src="playlist.cover" alt="Image" class="w-[100px] h-[100px] rounded-full items-center">
-     <audio controls>
+     <audio controls class="w-full mx-2">
        <source :src="playlist.audio" type="audio/mpeg">
      </audio> 
-     <p class="font-bold text-xl capitalize ">{{ playlist.title }}</p>
-     <p class="font-semibold text-lg capitalize">{{ playlist.artist }} <span class="italic">{{ playlist.year }}</span></p>
+     <p class=" font-semibold md:font-bold text-sm md:text-xl capitalize ">{{ playlist.title }}</p>
+     <p class="font-semibold text-xs capitalize">{{ playlist.artist }} <span class="italic">{{ playlist.year }}</span></p>
        </div>
     </div>
 </template>
@@ -18,5 +18,3 @@ export default{
     
 
 }</script>
-
-https://github.com/Yusuf-Sokunbi/audio-app.git
